7908659a298f963c194fe6fe0b3186cf1448effb
anhleh33 <166407355+anhleh33@users.noreply.github.com>
2025-05-20 09:39:12 +0700

    rechange main

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI, HTTPException
-from fastapi.responses import RedirectResponse  # <-- Add this import
 from starlette.responses import Response
 
 from app.db.models import UserAnswer
@@ -7,30 +6,38 @@ from app.api import api
 
 app = FastAPI()
 
-# Redirect root path to Swagger UI
-@app.get("/", include_in_schema=False)
+
+@app.get("/")
 def root():
-    return RedirectResponse(url="/docs")
+    return {"message": "Welcome you to"}
+
 
 @app.get("/user")
 def read_user():
     return api.read_user()
 
+
 @app.get("/question/{position}", status_code=200)
 def read_questions(position: int, response: Response):
     question = api.read_questions(position)
+
     if not question:
         raise HTTPException(status_code=400, detail="Error")
+
     return question
+
 
 @app.get("/alternatives/{question_id}")
 def read_alternatives(question_id: int):
     return api.read_alternatives(question_id)
 
+
 @app.post("/answer", status_code=201)
 def create_answer(payload: UserAnswer):
     payload = payload.dict()
+
     return api.create_answer(payload)
+
 
 @app.get("/result/{user_id}")
 def read_result(user_id: int):
