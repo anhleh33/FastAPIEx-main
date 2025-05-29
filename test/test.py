7dac39b28ba926a06f9c7a59bbc84c89b6fe8ea6
Anh Hoang <166407355+anhleh33@users.noreply.github.com>
2025-05-29 10:14:01 +0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -8,7 +8,7 @@ client = TestClient(app)
 def test_read_main():
     response = client.get('/')
     assert response.status_code == 200
-    assert response.json() == {'message': 'Welcome you to CK_DevOps 2025'}
+    assert response.json() == {'message': 'Fast API in Python'}
 
 
 def test_read_user():
@@ -18,12 +18,9 @@ def test_read_user():
 
 
 def test_read_question():
-    response = client.get('/question/2')
+    response = client.get('/question/1')
     assert response.status_code == 200
-    data = response.json()
-    assert 'position' in data, f"Key 'position' not found in response: {data}"
-    assert data['position'] == 1
-
+    assert response.json()['position'] == 1
 
 
 def test_read_question_invalid():
@@ -33,7 +30,7 @@ def test_read_question_invalid():
 
 
 def test_read_alternatives():
-    response = client.get('/alternatives/4')
+    response = client.get('/alternatives/1')
     assert response.status_code == 200
     assert response.json()[1]['question_id'] == 1
 
@@ -42,7 +39,7 @@ def test_create_answer():
     body = {"user_id": 1, "answers": [{"question_id": 1, "alternative_id": 2}, {
         "question_id": 2, "alternative_id": 2}, {"question_id": 2, "alternative_id": 2}]}
     body = json.dumps(body)
-    response = client.post('/answer', json=json.loads(body))
+    response = client.post('/answer', data=body)
     assert response.status_code == 201
 
 
